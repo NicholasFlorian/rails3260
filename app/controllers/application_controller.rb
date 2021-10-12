b4c7ee6724a8c551fe1a53ec2b1e40126d1cd1a8
Nick <nickjflorian@Gmail.com>
2021-10-12 19:58:27 -0400

    added sign-in route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
+    
+    # define the actions for our program
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file,
+  # see https://guides.rubyonrails.org/routing.html
+
+  # our main sign-in page
+  root "articles#sign_in"
+  get "/articles", to: "articles#sign_in"
+  
 end
