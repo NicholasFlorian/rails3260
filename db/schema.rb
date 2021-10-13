ccb4c0221fc94c97edf4d2e4c2403bad60bb8060
Nicholas Florian <nickjflorian@gmail.com>
2021-10-13 14:19:56 -0400

    fixed user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_13_010133) do
+ActiveRecord::Schema.define(version: 2021_10_13_181114) do
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.text "password"
+    t.string "name"
+    t.string "email"
+    t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
