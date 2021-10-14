dac590dc85cd8030355cb6c8a4f3c10de4a9b4a9
Nick <nickjflorian@Gmail.com>
2021-10-13 20:42:39 -0400

    added sign in session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -57,13 +57,14 @@ class UsersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
+  
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def user_params
-      params.require(:user).permit(:name, :email, :password)
-    end
+  # Only allow a list of trusted parameters through.
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
 
-  get 'sessions/sign_in'
   # include user pages
   resources :users
-  
-  get '/index', to: 'users#index"'
-  get '/create', to: 'users#index'
+  #get '/index', to: 'users#index"'
+  #get '/create', to: 'users#index'
+
+
+  # sign in session
+  get    'sessions/sign_in' to: 'sessions#sign_in'
+  post   '/login', to: 'sessions#create' 
+  delete '/logout', to: 'sessions#destroy'
+ 
   
   # set our root
-  root 'users#index'
-
+  root 'sessions#sign_in'
 end
