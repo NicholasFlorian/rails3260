e24ef4dd3111c9bc645838341e647f2fb6fa5437
Nick <nickjflorian@Gmail.com>
2021-10-20 19:10:16 -0400

    fixed profile Ui

--- config/routes.rb
+++ config/routes.rb
@@ -10,12 +10,12 @@ Rails.application.routes.draw do
   get     'register', to:     'sessions#register'
   post    'sign_in', to:      'sessions#sign_in' 
   post    'sign_up', to:      'sessions#sign_up'
-  delete  'sign_out', to:     'sessions#sign_out'
-  delete  'delete_user', to:  'sessions#delete_user'
 
 
   # user page
   get     'profile', to:    'sessions#profile'
+  delete  'sign_out', to:     'sessions#sign_out'
+  delete  'delete_user', to:  'sessions#delete_user'
 
   
   # set our root
